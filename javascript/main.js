20eacec4c8376af69f4081aa708d0d95a8c0b77f
atakriti <anwart256@gmail.com>
2022-04-16 13:52:43 +0200

    Updates

---
+++ javascript/main.js
@@ -0,0 +1,95 @@
+let one = () => {
+    let details = document.getElementById("details");
+    let plus = document.getElementById("plus");
+    let iX = document.getElementById("iX");
+    if (details.style.display == "none") {
+        details.style.display = "block"
+        plus.style.display = "none"
+        iX.style.display = "block"
+    } else {
+        details.style.display = "none"
+        plus.style.display = "block"
+        iX.style.display = "none"
+
+    }
+}
+// ====== two ========
+let two = () => {
+    let detailsTwo = document.getElementById("detailsTwo");
+    let plusTwo = document.getElementById("plusTwo");
+    let iXTwo = document.getElementById("iXTwo");
+    if (detailsTwo.style.display == "none") {
+        detailsTwo.style.display = "block"
+        plusTwo.style.display = "none"
+        iXTwo.style.display = "block"
+    } else {
+        detailsTwo.style.display = "none"
+        plusTwo.style.display = "block"
+        iXTwo.style.display = "none"
+
+    }
+}
+// ======= three =========
+let three = () => {
+    let detailsThree = document.getElementById("detailsThree");
+    let plusThree = document.getElementById("plusThree");
+    let iXThree = document.getElementById("iXThree");
+    if (detailsThree.style.display == "none") {
+        detailsThree.style.display = "block"
+        plusThree.style.display = "none"
+        iXThree.style.display = "block"
+    } else {
+        detailsThree.style.display = "none"
+        plusThree.style.display = "block"
+        iXThree.style.display = "none"
+
+    }
+}
+// ========= four ===========
+let four = () => {
+    let detailsFour = document.getElementById("detailsFour");
+    let plusFour = document.getElementById("plusFour");
+    let iXFour = document.getElementById("iXFour");
+    if (detailsFour.style.display == "none") {
+        detailsFour.style.display = "block"
+        plusFour.style.display = "none"
+        iXFour.style.display = "block"
+    } else {
+        detailsFour.style.display = "none"
+        plusFour.style.display = "block"
+        iXFour.style.display = "none"
+
+    }
+}
+// ============= five ============
+let five = () => {
+    let detailsFive = document.getElementById("detailsFive");
+    let plusFive = document.getElementById("plusFive");
+    let iXFive = document.getElementById("iXFive");
+    if (detailsFive.style.display == "none") {
+        detailsFive.style.display = "block"
+        plusFive.style.display = "none"
+        iXFive.style.display = "block"
+    } else {
+        detailsFive.style.display = "none"
+        plusFive.style.display = "block"
+        iXFive.style.display = "none"
+
+    }
+}
+// ============ six ========
+let six = () => {
+    let detailsSix = document.getElementById("detailsSix");
+    let plusSix = document.getElementById("plusSix");
+    let iXSix = document.getElementById("iXSix");
+    if (detailsSix.style.display == "none") {
+        detailsSix.style.display = "block"
+        plusSix.style.display = "none"
+        iXSix.style.display = "block"
+    } else {
+        detailsSix.style.display = "none"
+        plusSix.style.display = "block"
+        iXSix.style.display = "none"
+
+    }
+}
